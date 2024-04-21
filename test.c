eaded164a91442c260b3b51a21391ff40c1a8f2b
Theo <95878514+TheoW03@users.noreply.github.com>
2024-04-21 18:22:19 -0400

    link and run :D

--- test.c
+++ test.c
@@ -1,16 +1,15 @@
 
 #include <stdio.h>
-extern int test(int a, int b);
-int test_global = 20;
-extern int global_var;
-int add(int a, int b)
-{
-    return a + b;
+int add(int a, int b){
+    return a+ b;
 }
+extern int test(int a, int b);
 int main(int argc, char const *argv[])
 {
+    // pid_t a = fork();
     int s = test(1, 1);
     printf("%d \n",s);
     return 0;
     // float d = 4.14;
 }
+
