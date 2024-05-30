b6fbba3cd0f55431ff1024f8c10f48de418e4c1a
Theo <95878514+TheoW03@users.noreply.github.com>
2024-05-30 11:50:42 -0400

    added cmd line

--- src/LLVMCodeGen/IRCodeGen.cs
+++ src/LLVMCodeGen/IRCodeGen.cs
@@ -1,62 +1,90 @@
+using System.Diagnostics;
 using LLVMSharp;
 using LLVMSharp.Interop;
+
 public class IRCodeGen
 {
+    private static void LLVMCreateFile(LLVMModuleRef module, string file)
+    {
+        // Specify the directory path
+        string directoryPath = "out";
+        if (!Directory.Exists(directoryPath))
+        {
+            Directory.CreateDirectory(directoryPath);
+        }
 
-	// private static LLVMValueRef IntegerExpression(LLVMBuilderRef builder, LLVMModuleRef module)
-	// {
+        if (file == "")
+        {
+            file = "output.ll";
+        }
 
-	// }
-	private static void LLVMCreateFile(LLVMModuleRef module, string file)
-	{
-		// Specify the directory path
-		string directoryPath = "out";
-		if (!Directory.Exists(directoryPath))
-		{
-			Directory.CreateDirectory(directoryPath);
-		}
-		if (file == "")
-		{
-			file = "output.ll";
-		}
-		string filePath = Path.Combine(directoryPath, file);
-		File.WriteAllText(filePath, module.ToString());
-		Console.WriteLine("code successfully compiled");
-		module.Dispose();
+        string filePath = Path.Combine(directoryPath, file);
+        File.WriteAllText(filePath, module.ToString());
+        Console.WriteLine("code successfully compiled");
+        module.Dispose();
+    }
 
-	}
-	public static void LLVM_Gen(List<INode?> statements, string file)
-	{
-		var module = LLVMModuleRef.CreateWithName("main");
-		LLVMBuilderRef builder = module.Context.CreateBuilder();
-		Context c = new();
-		// Context context = new CO
-		#pragma warning disable CS8602 // Dereference of a possibly null reference.
-		foreach (INode? statement in statements)
-			statement.CodeGen(new CodeGenVisitor(), builder, module, c);
-		string directoryPath = "out";
-		if (!Directory.Exists(directoryPath))
-		{
-			Directory.CreateDirectory(directoryPath);
-		}
-		if (file == "")
-		{
-			file = "output.ll";
-		}
-		string filePath = Path.Combine(directoryPath, file);
-		File.WriteAllText(filePath, module.ToString());
-		Console.WriteLine("code successfully compiled");
-		Console.WriteLine("IR code gen file path: " + filePath);
-		// builder.BuildFPToSI
-		// builder.BuildTrunc()
+    public static void LLVM_Gen(List<INode?> statements, CompileOptions compileOptions)
+    {
+        var module = LLVMModuleRef.CreateWithName("main");
+        LLVMBuilderRef builder = module.Context.CreateBuilder();
+        Context c = new();
+        // Context context = new CO
+#pragma warning disable CS8602 // Dereference of a possibly null reference.
+        foreach (INode? statement in statements)
+            statement.CodeGen(new CodeGenVisitor(), builder, module, c);
+        // string directoryPath = "out";
+        // if (!Directory.Exists(directoryPath))
+        // {
+        //     Directory.CreateDirectory(directoryPath);
+        // }
 
-		// new FloatNode().Accept<FloatNode>(new FloatExprVis(), builder, module);
-		// LLVMTypeRef funcType = LLVMTypeRef.CreateFunction(LLVMTypeRef.Int32, new LLVMTypeRef[0] { }, false);
-		// LLVMValueRef function = module.AddFunction("main", funcType);
-		// LLVMBasicBlockRef entry = function.AppendBasicBlock("entry");
-		// builder.PositionAtEnd(entry);
-		// builder.BuildRet(expr.Accept(builder, module));
-		// Console.WriteLine(module.ToString());
+        // string filePath = Path.Combine(directoryPath, compileOptions.OutputFile);
+        // if (compileOptions.IrFile)
+        //     File.WriteAllText(filePath, module.ToString());
+        // Console.WriteLine("code successfully compiled");
+        // Console.WriteLine("IR code gen file path: " + filePath);
 
-	}
+        //outputting directly to an object file
+        //https://llvm.org/docs/tutorial/MyFirstLanguageFrontend/LangImpl08.html
+        var targetTriple = LLVMTargetRef.DefaultTriple;
+        var target = LLVMTargetRef.GetTargetFromTriple(targetTriple);
+        var cpu = "generic";
+        var features = "";
+        var opt = compileOptions.OptLevel;
+        var targetMachine = target.CreateTargetMachine(
+            targetTriple,
+            cpu,
+            features,
+            opt,
+            LLVMRelocMode.LLVMRelocPIC,
+            LLVMCodeModel.LLVMCodeModelLarge
+        );
+        var out_string = "";
+        if (!Directory.Exists("lacus-bin"))
+            Directory.CreateDirectory("lacus-bin");
+        targetMachine.TryEmitToFile(
+            module,
+            "lacus-bin/a.o",
+            LLVMCodeGenFileType.LLVMObjectFile,
+            out out_string
+        );
+        Process link = new Process();
+        link.StartInfo.FileName = "ld";
+        link.StartInfo.Arguments = $"lacus-bin/a.o -o {compileOptions.OutputFile}";
+        link.Start();
+        link.WaitForExit();
+        File.Delete("lacus-bin/a.o");
+        Directory.Delete("lacus-bin");
+        // builder.BuildFPToSI
+        // builder.BuildTrunc()
+
+        // new FloatNode().Accept<FloatNode>(new FloatExprVis(), builder, module);
+        // LLVMTypeRef funcType = LLVMTypeRef.CreateFunction(LLVMTypeRef.Int32, new LLVMTypeRef[0] { }, false);
+        // LLVMValueRef function = module.AddFunction("main", funcType);
+        // LLVMBasicBlockRef entry = function.AppendBasicBlock("entry");
+        // builder.PositionAtEnd(entry);
+        // builder.BuildRet(expr.Accept(builder, module));
+        // Console.WriteLine(module.ToString());
+    }
 }
--- test.c
+++ test.c
@@ -1,49 +1,9 @@
-
 #include <stdio.h>
-// extern volatile unsigned int a;
-char test(int a, int b);
+
 int main(int argc, char const *argv[])
 {
-    //     int d = 1;
-    //    volatile int b = d + 1;
-    //     b = 10;
-    char s = test(1, 279);
-    printf("%d \n",s);
 
-    // int i = 0;
-    // int a = argc;
-    // for( i = 0; i < a; i++){
-    //    add(i);
-    //    i = i * 2;
-    // }
-    // i += 10;
-    int d = 1;
-    d = d + 1;
-    // char a = '1' + 1;
-    d += argc;
-    return d;
-    // int b = 0;
-    // int a = 0; //a = 0
-    // a = b; //a's dependcy graah: "b"
-    // a = a + 1; // a: a, b
-    // printf("%d\n",s);
+    printf("hello world");
+    return 0;
 
-    // good wya todo this
-
-    // traverse the tree with perhaps a "Transform" method in Inode
-    //  and context Object
-
-    // if traversal sees a var reference. it will add it to the dependecy graph
-    // if it sees a var declaration it will create a new dependecy gaph
-    // immediates get added as "immediate"
-
-    // ex: int a = 10 + b; //assuming b is declard already
-    // a's dependecy graph would be
-    //  empty Node, node for b. with a var containing the depency graph for b
-    // it builds the graph until it gets to return
-
-    // at return. it goes through the list of vars/var references. if it doesnt turn up in the big traversal. it gets marked as dead code
-    //  and removed
-    //
-    //  return i;
 }
