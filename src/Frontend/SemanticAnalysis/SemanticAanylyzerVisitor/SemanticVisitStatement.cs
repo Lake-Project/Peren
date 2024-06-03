1d0fbd50ecbbea025fcdd4c84738aa0cf057fc46
Theo <95878514+TheoW03@users.noreply.github.com>
2024-06-02 20:39:19 -0400

    a

--- src/Frontend/SemanticAnalysis/SemanticAanylyzerVisitor/SemanticVisitStatement.cs
+++ src/Frontend/SemanticAnalysis/SemanticAanylyzerVisitor/SemanticVisitStatement.cs
@@ -16,14 +16,27 @@ public struct SemanticVar
 
 public class SemanticVisitStatement : StatementVisit
 {
+    public SemanticContext<SemanticVar> _Context { get; set; }
+
+    public SemanticVisitStatement()
+    {
+        _Context = new();
+    }
+
     public override void Visit(VaraibleDeclarationNode node)
     {
-        throw new NotImplementedException();
+        _Context.AddValue(
+            node.name,
+            new SemanticVar(new LacusType(TypeEnum.INTEGER), _Context.GetSize())
+        );
+        node.ExpressionNode.Visit(new SemanticVisitExpr(_Context, new LacusType(TypeEnum.INTEGER)));
     }
 
     public override void Visit(VaraibleReferenceStatementNode node)
     {
-        throw new NotImplementedException();
+        SemanticVar v = _Context.GetValue(node.name);
+        LacusType l = node.expression.Visit(new SemanticVisitExpr(_Context, v.VarType));
+        node.ScopeLocation = v.ScopeLocation;
     }
 
     public override void Visit(FunctionCallNode node)
@@ -33,7 +46,10 @@ public class SemanticVisitStatement : StatementVisit
 
     public override void Visit(FunctionNode node)
     {
-        throw new NotImplementedException();
+        _Context.AllocateScope();
+        node.Parameters.ForEach(n => n.Visit(this));
+        node.statements.ForEach(n => n.Visit(this));
+        _Context.DeallocateScope();
     }
 
     public override void Visit(ReturnNode node)
