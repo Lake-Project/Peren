d29a934bd3af07abc42e804b554975c66dac8cfd
Theo <95878514+TheoW03@users.noreply.github.com>
2024-07-19 17:45:31 -0400

    added Arrays and arr ref

--- src/Frontend/Parser/AST/ArrayRefNode.cs
+++ src/Frontend/Parser/AST/ArrayRefNode.cs
@@ -4,5 +4,5 @@ namespace LacusLLVM.Frontend.Parser.AST;
 
 public class ArrayRefNode(Tokens Name, INode elem) : VaraibleReferenceNode(Name)
 {
-    public INode Elem { get; set; } = elem;
+    public INode Elem { get; set; } = elem; //l
 }
