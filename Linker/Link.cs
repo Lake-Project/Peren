f5251a57724046e0c5b63307ebf83beb6d6df030
Theo <95878514+TheoW03@users.noreply.github.com>
2024-07-29 22:08:15 -0400

    rename

---
+++ Linker/Link.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using Linker;
+
+public class Link
+{
+    public static void LinkInvoke(string fileName)
+    {
+        LinkerRun.LinkCode(fileName);
+    }
+    // public static void Main(string[] args)
+    // {
+    //     Console.WriteLine("Hello, World!");
+    // }
+}
